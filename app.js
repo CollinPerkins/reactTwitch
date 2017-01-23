02bb45a2f4f1bbb8aeedec32c7738065e09a047c
Collin Perkins <collinperkinsweb@gmail.com>
2017-01-23 14:01:55 -0600

    api logic finished

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const path = require('path');
 const port = process.env.PORT || 3030;
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(express.static(__dirname + '/'));
 
 app.use(cors());
+
+// app.get('/getTwitchList', (req, res) => {
+//   var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+//   var userList = [];
+//   for (var i = 0; i < users.length; i++) {
+//     axios.get('https://api.twitch.tv/kraken/users/' + users[i] + '?client_id=pl279j6lfuewen5dotua3yvv6zx1ma')
+//     .then(function (response) {
+//       userList.push(response.data);
+//
+//       console.log(userList);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+//     console.log(userList);
+//   }
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
--- src/components/app.js
+++ src/components/app.js
@@ -1,37 +1,65 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import axios from 'axios';
+import Bluebird from 'bluebird';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      usersList: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"]
     }
+    this.renderUsers = this.renderUsers.bind(this);
   }
 
-  componentWillMount(){
-    // $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-    //   console.log(data);
-    // });
-    $.ajax({
-      url: `https://api.twitch.tv/kraken/streams/freecodecamp`,
-      jsonp: "callback",
-      dataType: "jsonp",
-      data: {
-        format: "json"
-      },
-      success: function( response ) {
-        // this.setState({
-        // })
-        console.log(response);
+  renderUsers() {
+    if (!this.state.userData || !this.state.userData.length) return;
+    console.log(this.state.userData);
+    return this.state.userData.map((user, index) => {
+      // console.log(this.state.usersList[index]);
+      if(user.error === 'error') {
+        return (
+          <div key={index}>
+            <h1>{this.state.usersList[index]}</h1>
+            <h3>This user does note exsist!</h3>
+          </div>
+        )
       }
-    });
+      if(user.data.stream === null){
+        return (
+          <div key={index}>
+            <h1>{this.state.usersList[index]}</h1>
+            <h3>Offline</h3>
+          </div>
+        )
+      }
+      return <h1 key={index}>{user.data.stream.channel.display_name}</h1>
+    })
+  };
+
+  componentWillMount(){
+    const userList = this.state.usersList.map((user) => {
+      return axios.get(`https://api.twitch.tv/kraken/users/${user}?client_id=pl279j6lfuewen5dotua3yvv6zx1ma`)
+      .then(function (response) {
+        console.log(response);
+        return axios.get(`https://api.twitch.tv/kraken/streams/${user}?client_id=pl279j6lfuewen5dotua3yvv6zx1ma`)
+      })
+      .catch(function (error) {
+        return {error: 'error'};
+      });
+    })
+
+    const users = Bluebird.all(userList)
+     .then((userData) => {
+       this.setState({userData: userData});
+     });
   }
 
   render() {
     return (
       <div>
-        <h1>Hi</h1>
+        {this.renderUsers()}
       </div>
     );
   }
